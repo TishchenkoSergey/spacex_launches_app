aebc5cdb679d4d4762fff9e57923b14f92a922b8
Serhii <tishchenko.seriy@gmail.com>
2025-09-07 05:32:07 +0300

    feat(data, domain): update di

--- packages/data/lib/di/injection.module.dart
+++ packages/data/lib/di/injection.module.dart
@@ -6,10 +6,24 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'dart:async' as _i687;
 
+import 'package:data/di/injection.dart' as _i237;
+import 'package:data/repository/launches_repository.dart' as _i584;
+import 'package:data/repository/rocket_repository.dart' as _i1003;
+import 'package:dio/dio.dart' as _i361;
+import 'package:domain/domain.dart' as _i494;
 import 'package:injectable/injectable.dart' as _i526;
 
 class DataPackageModule extends _i526.MicroPackageModule {
 // initializes the registration of main-scope dependencies inside of GetIt
   @override
-  _i687.FutureOr<void> init(_i526.GetItHelper gh) {}
+  _i687.FutureOr<void> init(_i526.GetItHelper gh) {
+    final registerModule = _$RegisterModule();
+    gh.lazySingleton<_i361.Dio>(() => registerModule.githubClient());
+    gh.factory<_i494.LaunchesRepository>(
+        () => _i584.LaunchesRepositoryImpl(dio: gh<_i361.Dio>()));
+    gh.factory<_i494.RocketRepository>(
+        () => _i1003.RocketRepositoryImpl(dio: gh<_i361.Dio>()));
+  }
 }
+
+class _$RegisterModule extends _i237.RegisterModule {}
