9990382893f48af44a183a2b3945d2a8d1288837
Serhii <tishchenko.seriy@gmail.com>
2025-09-07 05:33:24 +0300

    feat: analysis options files

--- packages/domain/lib/di/injection.module.dart
+++ packages/domain/lib/di/injection.module.dart
@@ -6,10 +6,18 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'dart:async' as _i687;
 
+import 'package:domain/domain.dart' as _i494;
+import 'package:domain/usecases/get_launches_list_use_case.dart' as _i1050;
+import 'package:domain/usecases/get_rockets_info_use_case.dart' as _i646;
 import 'package:injectable/injectable.dart' as _i526;
 
 class DomainPackageModule extends _i526.MicroPackageModule {
 // initializes the registration of main-scope dependencies inside of GetIt
   @override
-  _i687.FutureOr<void> init(_i526.GetItHelper gh) {}
+  _i687.FutureOr<void> init(_i526.GetItHelper gh) {
+    gh.factory<_i646.GetRocketsInfoUseCase>(
+        () => _i646.GetRocketsInfoUseCaseImpl(gh<_i494.RocketRepository>()));
+    gh.factory<_i1050.GetLaunchesListUseCase>(() =>
+        _i1050.GetLaunchesListUseCaseImpl(gh<_i494.LaunchesRepository>()));
+  }
 }
